44d0e4cad708ddb5a23e58268434bbbd64f4787d
Jose Cruz Moreira Galvan <jmoreira@utzac.edu.mx>
2024-03-01 15:58:09 -0600

    start

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const logger = require('morgan');
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD || ''
-const DB_NAME = process.env.DB_NAME || 'mydb'
-const DB_PORT = process.env.DB_PORT || 3306
+const PORT = process.env.PORT
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER 
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+const DB_PORT = process.env.DB_PORT
 
 const conn = require('mysql2');
 const bodyParser = require('body-parser');
